e006296d013c0f1c87e9cf1051229a535bf480cb
dishi <dishi.gautam@joshtechnologygroup.com>
2026-08-24 15:57:17 +0530

    [DG_A1_03]: Add travel point section and styling added across all layouts

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,5 @@
-import '../styles/main.scss';
+import '@styles/main.scss'
+import '@styles/layout/_header.scss'
+import '@styles/layout/_hero.scss'
+import '@styles/layout/_travel-point.scss'
 import '../script/navigation.js';
