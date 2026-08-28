f54b4ac8a8ffcba5432e3389c2f7a3e06b812836
dishi <dishi.gautam@joshtechnologygroup.com>
2026-08-28 16:29:16 +0530

    [DG_A1_03]: Update spacing, font update and path aliasing

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,2 @@
-import '@styles/main.scss'
-import '@styles/layout/_header.scss'
-import '@styles/layout/_hero.scss'
-import '@styles/layout/_travel-point.scss'
+import '../styles/main.scss';
 import '../script/navigation.js';
